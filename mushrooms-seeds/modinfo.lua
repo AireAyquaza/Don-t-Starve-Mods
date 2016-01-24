35bda526b7c2106f23fccecf4353115ca0bad8c4
Aire Ayquaza <aireayquaza@hotmail.fr>
2016-01-24 16:03:40 +0100

    Mushrooms Seeds
    Add mushrooms seeds for plant them into farms to get mushrooms farms.
    Give a mushroom to a bird in a birdcage to get mushrooms seeds!

---
+++ mushrooms-seeds/modinfo.lua
@@ -0,0 +1,18 @@
+name = "Mushrooms Seeds"
+description = "Give mushrooms to a bird in a birdcage to get mushrooms seeds, plants them to a farm to get mushrooms. [WIP] I need help to set a texture for the seeds result in farm."
+author = "Aire Ayquaza"
+version = "1.0.0"
+
+forumthread = ""
+
+api_version = 6
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+dst_compatible = false
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
+server_filter_tags = {"mushrooms","mushroom","seeds","seed","farm"}
